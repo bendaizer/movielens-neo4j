df40a1438d6ab848e9b70f91e44c59e6b91b2397
Amine Benhenni <abenhenni.kernix@gmail.com>
2013-04-17 17:44:48 +0200

    import of nodes in neo4j, with corresponding indexes

--- ImportML.java
+++ ImportML.java
@@ -49,12 +49,12 @@ public class ImportML{
     public static void main(String[] args) {
         deleteFileOrDirectory(db_directory);
 
-        BatchInserter inserter = BatchInserters.inserter( DB_PATH );
-        BatchInserterIndexProvider indexProvider = new LuceneBatchInserterIndexProvider( inserter );
-        BatchInserterIndex user_index = indexProvider.nodeIndex("users", MapUtil.stringMap("type", "exact"));
-        BatchInserterIndex movie_index = indexProvider.nodeIndex("movies", MapUtil.stringMap("type", "exact"));
-        BatchInserterIndex genre_index = indexProvider.nodeIndex("genres", MapUtil.stringMap("type", "exact"));
-        BatchInserterIndex occupation_index = indexProvider.nodeIndex("occupation", MapUtil.stringMap("type", "exact"));
+        // BatchInserter inserter = BatchInserters.inserter( DB_PATH );
+        // BatchInserterIndexProvider indexProvider = new LuceneBatchInserterIndexProvider( inserter );
+        // BatchInserterIndex user_index = indexProvider.nodeIndex("users", MapUtil.stringMap("type", "exact"));
+        // BatchInserterIndex movie_index = indexProvider.nodeIndex("movies", MapUtil.stringMap("type", "exact"));
+        // BatchInserterIndex genre_index = indexProvider.nodeIndex("genres", MapUtil.stringMap("type", "exact"));
+        // BatchInserterIndex occupation_index = indexProvider.nodeIndex("occupation", MapUtil.stringMap("type", "exact"));
 
         try{
 
@@ -82,21 +82,74 @@ public class ImportML{
                                             );
             movies_list = importFile(ITEM_FILE, movie_label);
 
-            data_list = importFile(DATA_FILE,new ArrayList<String>(Arrays.asList("user_id","movie_id","rating","timestamp"));
+            data_list = importData(DATA_FILE,new ArrayList<String>(Arrays.asList("user_id","movie_id","rating","timestamp")));
 
-            for ( Map m : occupations_list){
-                System.out.println(m);
-            }
+
+            /*******/
+
+            Map<String,Long> user_id_node = new HashMap<String, Long>();
+            Map<String,Long> movie_id_node = new HashMap<String, Long>();
+            Map<String,Long> genre_node = new HashMap<String, Long>();
+            Map<String,Long> occupation_node = new HashMap<String, Long>();
+
+            occupation_node = node_inserter(occupations_list, "occupations", "occupation");
+            genre_node = node_inserter(genres_list, "genres", "genre");
+            user_id_node = node_inserter(users_list, "users", "user_id");
+            movie_id_node = node_inserter(movies_list, "movies", "movie_id");
+
+
+            // for ( Map m : data_list){
+            //     System.out.println(m);
+            // }
 
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.exit(1);
         }finally{
-            indexProvider.shutdown();
-            inserter.shutdown();
+            // indexProvider.shutdown();
+            // inserter.shutdown();
         }
     }
+
+
+    private static Map<String,Long> node_inserter(List<HashMap<String,Object>> nodes_map, String indexName, String indexProperty){
+        /****************************
+         Take list of properties, insert the corresponding nodes
+         returns a map between ID or type and nodeID for relationships
+         ****************************/
+
+        System.out.println("----");
+        System.out.println("Inserting nodes "+indexName);
+
+        Map<String,Long> property_id = new HashMap<String, Long>();
+        BatchInserter inserter = BatchInserters.inserter( DB_PATH );
+        BatchInserterIndexProvider indexProvider = new LuceneBatchInserterIndexProvider( inserter );
+
+        BatchInserterIndex theIndex = indexProvider.nodeIndex(indexName, MapUtil.stringMap("type", "exact"));
+        theIndex.setCacheCapacity(indexProperty,500000);
+
+
+        // Map<String,Object> result_map = new HashMap<String,Object>();
+        int size=0;
+
+        for (Map<String, Object> map : nodes_map) {
+
+                long node = inserter.createNode(map);
+                theIndex.add(node,map);
+                property_id.put(map.get(indexProperty).toString(),node);
+
+            size++;
+        }
+
+        indexProvider.shutdown();
+        inserter.shutdown();
+        System.out.println("size of import : "+size);
+
+        return property_id;
+
+    }
+
 
     private static List<HashMap<String,Object>> importFile(String file, ArrayList<String> labels){
         FileInputStream fstream = null;
@@ -144,6 +197,60 @@ public class ImportML{
         return nodes_list;
     }
 
+    private static List<HashMap<String,Object>> importData(String file, ArrayList<String> labels){
+        /*
+        Separate import method for u.data for 2 reasons:
+        + u.data is tab separated whilst others are "|" separated
+        + method will convert ratings to like/dislike at the same time
+        */
+        FileInputStream fstream = null;
+        BufferedReader reader = null;
+        HashMap<String, Object> properties = new HashMap<String, Object>();
+        List<HashMap<String,Object>> nodes_list = new ArrayList<HashMap<String,Object>>();
+
+        try{
+            fstream = new FileInputStream(file);
+            reader = new BufferedReader(new InputStreamReader(fstream, "UTF-8"));
+            String line;
+            String resource_name;
+            String[] tokens;
+            //FIXME should compare labels_size with number of columns in file at come point!
+            int num_column = labels.size();
+
+
+            while ((line = reader.readLine()) != null){
+                try{
+                    tokens = line.split("\\t");
+                    properties = new HashMap();
+                    for (int i=0; i<num_column ; i++){
+                        if (labels.get(i).equals("rating")){
+                            tokens[i] = Integer.parseInt(tokens[i])>2 ? "LIKE" : "DISLIKE";
+                        }
+                        properties.put(labels.get(i),tokens[i]);
+                    }
+                    nodes_list.add(properties);
+
+                }catch (Exception e){
+                    logger.warning("Import of file "+file+" - Bad Line  :"+line);
+                    logger.warning(e.getMessage());
+                }
+
+            }
+
+
+        } catch (Exception e) {
+            logger.warning("couldn't import file "+file);
+            logger.warning(e.getMessage());
+        }finally{
+            try{
+                fstream.close();
+                reader.close();
+            }catch(Exception e){
+            }
+        }
+
+        return nodes_list;
+    }
 
     private static void registerShutdownHook( final GraphDatabaseService graphDb )
     {
